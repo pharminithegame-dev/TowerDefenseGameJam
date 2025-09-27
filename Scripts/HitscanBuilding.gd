84896f70a39f9fa1734761948106e86f3670312b
Eyan11 <eyanmartucci11@gmail.com>
2025-09-26 17:06:34 -0700

    Hitscan Building and Raycast Updates
    - Adjusted raycast target position y coordinate (height) to account for situations where building and enemy do not have similar y coordinate values
    - Added a height offset node to the hitscan building
    - Added an activate_building and deactivate_building function that will start or stop the building targeting/shooting
    - Made the second collision layer for enemies and set enemies to collision layers 1 and 2. Also updated projectile and ray masks to 2 so only enemies collide.
    - Created a duplicate of the main scene for testing called EyanMainDemoScene

--- Scripts/HitscanBuilding.gd
+++ Scripts/HitscanBuilding.gd
@@ -1,15 +1,14 @@
 extends Node3D
 
 ### References
-@export var rot_target: Node3D
+@export var rot_node: Node3D
 @export var area_3d: Area3D
 @export var collision_shape: CollisionShape3D
-@export var projectile_tran: Node3D
 @export var raycast: RayCast3D
 @export var audio_player: AudioStreamPlayer
 
 ### Stats
-@export var projectile_damage := 20.0
+@export var attack_damage := 20.0
 @export var attack_range := 80.0
 @export var attack_cooldown := 2.0
 @export var sell_value := 150.0
@@ -17,7 +16,7 @@ extends Node3D
 ### Private Variables
 var attack_timer := 0.0
 var attack_range_sqr : float
-
+var is_active := true   # Enabled when building can target/shoot
 
 
 ### Initialize Node
@@ -40,35 +39,49 @@ func _ready() -> void:
 ### Starts attack on interval
 func _process(delta: float) -> void:
 	
-	attack_timer -= delta
-	if attack_timer <= 0:
-		# Start attack
-		attack_timer = attack_cooldown
-		shoot_raycast()
+	if is_active:   # Only target/shoot when active
+		attack_timer -= delta
+		
+		if attack_timer <= 0:   # Check to attack
+			attack_timer = attack_cooldown
+			shoot()
 
 
 ### Enables the raycast in the direction of the leading enemy 
 ### 	and applies damage to enemy if hit
-func shoot_raycast() -> void:
+func shoot() -> void:
 	
 	# Get nearest enemy in range
 	var target: Node3D = get_leading_enemy()
 	if target == null:
 		print("No Targets in Range For Building: ", name)
 		return
+	else:
+		print("Found target with name: ", name)
 	
 	# Rotate ProjectileSpawnRotation towards target
 	rotate_to_target(target.global_position)
 	
 	
 	### Raycast Towards Target
-	raycast.enabled = true
+	# Adjust raycast target height in case of a height difference between building and enemy
+	raycast.target_position.y = target.global_position.y - raycast.global_position.y
+	
+	#raycast.enabled = true
 	raycast.force_raycast_update()
+	
+	#print("----------")
+	#print("Enemy position: ", target.global_position)
+	#print("Raycast position: ", raycast.global_position)
+	#print("Raycast target position: ", raycast.target_position)
+	#print("Raycast forward vector: ", -raycast.global_transform.basis.z)
+	#print("Rotation Node forward vector: ", -rot_node.global_transform.basis.z)
+	#print("----------")
 	
 	# Make sure raycast collides with an enemy
 	if !raycast.is_colliding():
 		print("Got a target in range, but raycast is not colliding with anything")
-		raycast.enabled = false
+		#raycast.enabled = false
 		return
 	
 	# Make sure colliding object is an enemy
@@ -77,12 +90,13 @@ func shoot_raycast() -> void:
 		print("Got a raycast target, but target's collider is not in 'enemies' group")
 		return
 	
-	# TODO - apply damage
-	print("Applying hitscan damage to enemy")
-	audio_player
-	raycast.enabled = false
-	
-	audio_player.play()   # Play hitscan shoot sfx
+	# Apply damage to enemy
+	if collider.has_method("take_damage"):
+		audio_player.play()   # Play hitscan shoot sfx
+		collider.take_damage(attack_damage)
+		print("Applied ", attack_damage, " damage to enemy")
+		
+	#raycast.enabled = false
 
 
 ### Returns the enemy who is furthest along in enemy path.
@@ -103,7 +117,7 @@ func get_leading_enemy() -> Node3D:
 	for enemy in all_enemies:
 		# If this is the new closest enemy and is in attack range
 		var cur_dist_sqr := global_position.distance_squared_to(enemy.global_position)
-		if cur_dist_sqr < closest_dist_sqr and cur_dist_sqr < attack_range_sqr:   # NOTE - range check is redundant now, but I will need it when I remove Area3D later
+		if cur_dist_sqr < closest_dist_sqr:
 			closest_enemy = enemy
 			closest_dist_sqr = cur_dist_sqr
 	
@@ -113,16 +127,22 @@ func get_leading_enemy() -> Node3D:
 ### Rotates the ProjectileSpawnRotation node toward target
 func rotate_to_target(target_pos: Vector3) -> void:
 	
-	# Get 2D direction from building to target
-	var building_pos_2d := Vector2(global_position.x, global_position.z)
-	var target_pos_2d := Vector2(target_pos.x, target_pos.z)
-	var direction: Vector2 = target_pos_2d - building_pos_2d
-	
-	# Rotate on y axis in direction
-	rot_target.rotation.y = atan2(direction.x, direction.y)
+	target_pos.y = rot_node.global_position.y  # Ignore target y pos
+	rot_node.look_at(target_pos, Vector3.UP)
 
 
 ### Despawns building and returns the sell value
 func sell_building() -> float:
 	queue_free()   # Despawn building
 	return sell_value
+
+
+### Allows building to start targeting and shooting
+func activate_building() -> void:
+	attack_timer = attack_cooldown   # Reset attack timer
+	is_active = true
+
+
+### Stops building from targeting and shooting
+func deactivate_building() -> void:
+	is_active = false
