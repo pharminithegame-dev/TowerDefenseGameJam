f7ce776879541c17ab7034717314ed595d83d53d
IbbitF <alopez10abc@gmail.com>
2025-10-13 17:53:26 -0700

    UI VIsual Feedback + Home Base
    Upon alteration of health, icon flashes red to indicate change.
    
    Upon Money Gain, icon flashes green. Upon Money Loss, icon flashes yellow.
    
    Updated HUD manager for room to emit other needed signals.
    
    Added primitive home base.

--- Scripts/UI/HudManager.gd
+++ Scripts/UI/HudManager.gd
@@ -5,6 +5,13 @@ extends CanvasLayer
 @export var score_label: Label
 @export var money_label: Label
 
+# Visual Feedback
+@onready var health_flash: TextureRect = $HBoxContainer/HealthHBox/HealthTextureRect
+#Money Flash and Value Holder
+@onready var money_flash: TextureRect = $HBoxContainer/MoneyHBox/MoneyTextureRect
+var previous_money: int
+#score var
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,6 +26,11 @@ func _ready() -> void:
 	# TODO - connect score signal game manager
 	GameManager.health_altered.connect(set_health_label_text)
 	MoneyManager.money_altered.connect(set_money_label_text)
+	# Visual Signals
+	GameManager.health_altered.connect(_on_health_altered)
+	MoneyManager.money_altered.connect(_on_money_altered)
+	previous_money = MoneyManager.get_money()
+	
 
 
 # Update the health label text to new health value
@@ -34,3 +46,21 @@ func set_score_label_text(new_score: int) -> void:
 # Update the money label text to new money count
 func set_money_label_text(new_money: int) -> void:
 	money_label.text = str(new_money)
+
+# Signal calls
+# Health Flash (despite not using new_health, needs it for call)
+func _on_health_altered(new_health: int) -> void: 
+	flash_color(health_flash, Color(1, 0, 0, 1))
+
+func _on_money_altered(new_money: int) -> void:
+	if(new_money > previous_money):
+		flash_color(money_flash, Color(0,1,0,1))
+	elif(new_money < previous_money):
+		flash_color(money_flash, Color(1,1,0,1))
+	previous_money = new_money
+
+#Allows for flash of any color
+func flash_color(icon: TextureRect, color: Color) -> void:
+	icon.modulate = color
+	await get_tree().create_timer(0.2).timeout
+	icon.modulate = Color(1, 1, 1, 1)  # reset to normal
